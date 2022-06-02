a1eb56a5ffa6fb0cb8d19a5328f115c2c4c5fcf7
HyeJuSeon <hyejooseon@gmail.com>
2022-06-02 17:29:21 +0900

    docs : Add not landmarks

--- ai/utils/filtering.py
+++ ai/utils/filtering.py
@@ -20,14 +20,12 @@ def is_not_landmark(name):
 def main():
     global NOT_LANDMARKS
     load_dotenv(dotenv_path=os.getcwd() + '\\ai\\.env')
-    print(os.environ['DATASET'])
-    print(os.environ['FILENAME'])
     path = os.environ['DATASET']
     dirnames = os.listdir(path)
     NOT_LANDMARKS = load_not_landmarks(os.environ['FILENAME'])
     dlt_dirnames = list(filter(is_not_landmark, dirnames))
-    # print('=' * 00)
-    # print(set(dirnames) - set(dlt_dirnames))
+    print('=' * 00)
+    print(set(dirnames) - set(dlt_dirnames))
     print(len(dirnames))
     for dlt_dirname in dlt_dirnames:
         dir_path = path + dlt_dirname
