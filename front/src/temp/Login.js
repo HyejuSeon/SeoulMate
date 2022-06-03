162bf7f75078ab384e3f661c734b08d3cd414a41
pjs <vkrwltn12@gmail.com>
2022-06-03 16:59:20 +0900

    feat login

--- front/src/temp/Login.js
+++ front/src/temp/Login.js
@@ -1,27 +1,36 @@
-import { useContext, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import * as Api from '../api.js'
+import { useContext, useState } from "react";
+import {Box} from '@mui/material';
+import { Link , useNavigate } from "react-router-dom";
+import * as Api from '../api';
 import { DispatchContext } from "../App.js";
+import {ROUTES} from '../Route'
+import styled from 'styled-components';
+import style from "../srcAssets/style/Login.moudule.css";
+import CssTextField from "../components/user/CssTextField";
+import { visuallyHidden } from "@mui/utils";
+
+
+
 
 function Login(){
     const navigate = useNavigate()
     const dispatch = useContext(DispatchContext);
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
 
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })  
     const handleSubmit = async (e) => {
         e.preventDefault()
 
+
         try {
-            const res = await Api.post("users/login", {
-                email,
-                password,
-            })
-            console.log(res.data)
-            const user = res.data
+            //user 로그인 api 호출!
+            const res = await Api.post("users/login", form)
+            const user = res.data.loginUser
+
             const jwtToken = user.token
             sessionStorage.setItem("userToken", jwtToken)
-            alert("성공! 유저토큰 설정 완료!")
 
             dispatch({
                 type: "LOGIN_SUCCESS",
@@ -35,20 +44,84 @@ function Login(){
             alert(error.response.data)
         }
     }
-    return (
-        <>
-        <div>로그인 페이지</div>
-        <form onSubmit={handleSubmit}>
-            <div>Email</div>
-            <input type="text"
-                onChange={(e) => setEmail(e.target.value)}></input>
-            <div>password</div>
-            <input type="text"
-                onChange={(e) => setPassword(e.target.value)}></input>
-            <button type="submit">로그인하기</button>
-        </form>
-        <button onClick={() => navigate("/register")}>회원가입하러 가기</button>
-        </>
+    return(
+        <LoginBody onSubmit={handleSubmit}>
+            <LoginBodyUpper>
+                <Box>
+                </Box>
+                <Box>
+                    <LoginTitle>Log In</LoginTitle>
+                    <LoginTitle2>LandMark<span style={{color: "#BBD6FF"}}>travle</span></LoginTitle2>
+                </Box>
+            </LoginBodyUpper>
+            
+            <Box style={{textAlign:"center", marginTop:"20vh"}}>
+                <CssTextField
+                    style = {{width: '30%'}}
+                    id="email" 
+                    name="email"
+                    label="Email" 
+                    placeholder='Email'
+                    variant="standard"
+                    InputLabelProps={{
+                        style: {color: '#BBD6FF'}
+                    }}
+                    required   
+                    onChange={(e) => setForm((prev) => ({
+                        ...prev, [e.target.name]: e.target.value
+                    }))}            
+                />
+            </Box>
+
+            <Box style={{textAlign:"center", marginTop:"12vh"}}>
+                <CssTextField
+                    style = {{width: '30%'}}
+                    id="standard-basic"
+                    label="Password" 
+                    name="password"
+                    type='password'
+                    placeholder='Password'
+                    variant="standard" 
+                    InputLabelProps={{
+                        style: {color: '#BBD6FF'}
+                    }}
+                    required
+                    onChange={(e) => setForm((prev) => ({
+                        ...prev, [e.target.name]: e.target.value
+                    }))}  
+                />
+            </Box>
+
+            <div style={{display: "flex", flexDirection: "column", alignItems: "center", marginTop:"5vh"}}>
+                <button type='submit' style={{width: "30vw", borderRadius: "50px", border: "none", backgroundColor: "#BBD6FF", color: "white", padding: "5px", fontSize:"x-large" , cursor: "pointer" }}>LOG IN</button>
+
+            </div>
+            
+        </LoginBody>
     )
 }
-export default Login
+
+export default Login;
+
+const LoginBody = styled.form`
+`;
+
+const LoginBodyUpper = styled.div`
+    display: flex;  
+`;
+
+const LoginTitle = styled.div`
+    font-size: 20px;
+    font-weight: 600;
+    padding-top: 14px;
+`;
+const LoginTitle2 = styled.div`
+    font-size: 18px;
+    font-weight: 600;
+    padding-top: 4px;
+`;
+
+const LogoImg = styled.img`
+    width: 60px;
+    cursor: pointer;
+`;
