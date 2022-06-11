0a563062126e127fb532ac209fa2bd5b5f8bbe6a
HyeJuSeon <hyejooseon@gmail.com>
2022-06-12 01:32:38 +0900

    refact : Refactor cnt_img

--- ai/utils/eda.py
+++ ai/utils/eda.py
@@ -7,25 +7,32 @@ f_path = "C:/windows/Fonts/malgun.ttf"
 font_manager.FontProperties(fname=f_path).get_name()
 rc('font', family='Malgun Gothic')
 
-# Excel
-load_dotenv(dotenv_path=os.getcwd() + '\\ai\\.env')
-path = os.environ['DATASET_PATH']
-dirs = os.listdir(path)
-data = []
-for dir in dirs:
-    landmarks = os.listdir(path + dir)
-    data.append([dir, len(landmarks)])
-df = pd.DataFrame(data, columns=['name', 'img_num'])
-df.to_excel('ai/eda.xlsx', index=False)
+def cnt_img():
+    # Excel
+    path = os.environ['DATASET_PATH']
+    dirs = os.listdir(path)
+    data = []
+    for dir in dirs:
+        landmarks = os.listdir(path + dir)
+        data.append([dir, len(landmarks)])
+    df = pd.DataFrame(data, columns=['name', 'img_num'])
+    df.to_excel('ai/eda.xlsx', index=False)
+    df = pd.read_excel('ai/eda.xlsx')
+    img_num = df['img_num']
 
-df = pd.read_excel('ai/eda.xlsx')
-img_num = df['img_num']
-# 최소, 최대, 평균값
-print(min(img_num), max(img_num), img_num.mean())
-df = df.sort_values('img_num')
-plt.figure(figsize=(20, 10))
-plt.bar(df['name'], df['img_num'])
-plt.title('랜드마크 별 프레임 개수')
-plt.xticks(rotation=90, fontsize=6)
-plt.savefig('ai/imgs/frame_num_per_landmark.png')
-# plt.show()
+    # 최소, 최대, 평균값
+    print(min(img_num), max(img_num), img_num.mean())
+    df = df.sort_values('img_num')
+    plt.figure(figsize=(20, 10))
+    plt.bar(df['name'], df['img_num'])
+    plt.title('랜드마크 별 프레임 개수')
+    plt.xticks(rotation=90, fontsize=6)
+    plt.savefig('ai/imgs/frame_num_per_landmark.png')
+    # plt.show()
+
+def main():
+    load_dotenv(dotenv_path=os.getcwd() + '\\ai\\.env')
+    cnt_img()
+
+if __name__ == '__main__':
+    main()
