d4ab93fa791e9512313f7198599932e10f8ce4c1
HyeJuSeon <hyejooseon@gmail.com>
2022-06-14 13:52:20 +0900

    feat : Add category eda

--- ai/utils/eda.py
+++ ai/utils/eda.py
@@ -1,11 +1,30 @@
 import os
 import pandas as pd
+from tqdm import tqdm
+import json
+from collections import defaultdict
+import pickle
 from dotenv import load_dotenv
 from matplotlib import pyplot as plt
 from matplotlib import rc, font_manager
 f_path = "C:/windows/Fonts/malgun.ttf"
 font_manager.FontProperties(fname=f_path).get_name()
 rc('font', family='Malgun Gothic')
+
+def dump_pkl(obj, filename):
+    with open(filename, 'wb') as fw:
+        pickle.dump(obj, fw)
+
+def load_pkl(filename):
+    with open(filename, 'rb') as fr:
+        return pickle.load(fr)
+
+def bar_plot(x, y, filename, title='', fontsize=10):
+    plt.figure(figsize=(20, 10))
+    plt.bar(x, y)
+    plt.title(title)
+    plt.xticks(rotation=90, fontsize=fontsize)
+    plt.savefig(filename)
 
 def cnt_img():
     # Excel
@@ -23,16 +42,53 @@ def cnt_img():
     # 최소, 최대, 평균값
     print(min(img_num), max(img_num), img_num.mean())
     df = df.sort_values('img_num')
-    plt.figure(figsize=(20, 10))
-    plt.bar(df['name'], df['img_num'])
-    plt.title('랜드마크 별 프레임 개수')
-    plt.xticks(rotation=90, fontsize=6)
-    plt.savefig('ai/imgs/frame_num_per_landmark.png')
-    # plt.show()
+    bar_plot(df['name'], df['img_num'], 'ai/imgs/frame_num_per_landmark.png', '랜드마크 별 프레임 개수', 6)
+
+def category():
+    path = os.environ['LABELS']
+    dirs = os.listdir(path)
+    locations = defaultdict(set)
+    supercategories = defaultdict(set)
+    subcategories = defaultdict(set)
+    for dir in tqdm(dirs):
+        label = os.listdir(path + dir)[0]
+        with open(f'{path}{dir}/{label}', "r", encoding='utf-8') as jsonfile:
+            anno = json.load(jsonfile)
+        category = anno['categories'][0]['metainfo']
+        location = category['location2']
+        supercategory = category['Type1']
+        subcategory = category['Type2']
+        locations[location].add(dir)
+        supercategories[supercategory].add(dir)
+        subcategories[subcategory].add(dir)
+
+    dump_pkl(locations, 'ai/data/locations.pkl')
+    dump_pkl(supercategories, 'ai/data/supercategories.pkl')
+    dump_pkl(subcategories, 'ai/data/subcategories.pkl')
+
+def draw_category(dict, filename, title='', fontsize=10):
+    category, classes = zip(*dict.items())
+    classes = [len(c) for c in classes]
+    # 최소, 최대, 평균값
+    print(min(classes), max(classes), sum(classes) / len(classes))
+    bar_plot(category, classes, filename, title, fontsize)
+
+def cnt_category():
+    locations = load_pkl('ai/data/locations.pkl')
+    supercategories = load_pkl('ai/data/supercategories.pkl')
+    subcategories = load_pkl('ai/data/subcategories.pkl')
+    print('location')
+    draw_category(locations, 'ai/imgs/class_num_per_location.png', '장소 별 클래스 개수', 12)
+    print('supercategory')
+    draw_category(supercategories, 'ai/imgs/class_num_per_supercategory.png', 'supercategory 별 클래스 개수', 12)
+    print('subcategory')
+    draw_category(subcategories, 'ai/imgs/class_num_per_subcategory.png', 'subcategory 별 클래스 개수', 12)
 
 def main():
     load_dotenv(dotenv_path=os.getcwd() + '\\ai\\.env')
     cnt_img()
+    category()
+    cnt_category()
 
 if __name__ == '__main__':
     main()
