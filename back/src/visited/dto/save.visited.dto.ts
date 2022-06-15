005500963ae5adc2f7f34393a4a47e8030e8e928
hongloans <loans93@gmail.com>
2022-06-15 13:35:25 +0900

    modify: modify dtos

--- back/src/visited/dto/save.visited.dto.ts
+++ back/src/visited/dto/save.visited.dto.ts
@@ -1,21 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class saveVisitedDto {
     @ApiProperty({
         description: 'image url',
         required: false,
     })
+    @IsString()
     landmark_img: string;
 
     @ApiProperty({
         description: 'landmark_id',
         required: true,
     })
+    @IsNumber()
     landmark_id: number;
 
     @ApiProperty({
         description: 'user_id',
         required: true,
     })
+    @IsString()
     user_id: string;
 }
