e09d6f8a002e11a46a1719adddea97a8d48d267e
hongloans <loans93@gmail.com>
2022-06-15 13:34:45 +0900

    feat: use pipe and image put api

--- back/src/visited/visited.controller.ts
+++ back/src/visited/visited.controller.ts
@@ -5,13 +5,17 @@ import {
     HttpStatus,
     Param,
     Post,
+    Put,
     Query,
     Res,
+    UsePipes,
+    ValidationPipe,
 } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { saveVisitedDto } from './dto/save.visited.dto';
 import { returnVisitedDto } from './dto/return.visited.dto';
 import { VisitedService } from './visited.service';
+import { updateVisitedDto } from './dto/update.visited.dto';
 
 @ApiTags('visited')
 @Controller('visited')
@@ -35,6 +39,7 @@ export class VisitedController {
 
     @Post('/')
     @ApiOperation({ summary: '방문지 등록' })
+    @UsePipes(ValidationPipe)
     @ApiBody({ type: saveVisitedDto })
     @ApiResponse({
         status: 200,
@@ -45,6 +50,24 @@ export class VisitedController {
         @Body() visitedDto: saveVisitedDto,
     ): Promise<void> {
         const visited = await this.visitedService.create(visitedDto);
+        res.status(HttpStatus.OK).json(visited);
+    }
+
+    @Put('/image')
+    @ApiOperation({
+        summary:
+            '방문지 사진 등록, 이미 등록된 방문지 데이터에 사진만 추가하고 싶은 경우 사용',
+    })
+    @ApiBody({ type: updateVisitedDto })
+    @ApiResponse({
+        status: 200,
+        description: 'Image successfully updated',
+    })
+    async imageUpload(
+        @Res() res: any,
+        @Body() visitedDto: updateVisitedDto,
+    ): Promise<void> {
+        const visited = await this.visitedService.update(visitedDto);
         res.status(HttpStatus.OK).json(visited);
     }
 }
