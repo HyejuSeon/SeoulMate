61c494e337dac2fa4313154066276176f5d5fa1a
MongLong0214 <weplay0628@gmail.com>
2022-06-17 19:44:45 +0900

    feat: 게시판 구현중

--- front/src/components/upload/UploadResult.js
+++ front/src/components/upload/UploadResult.js
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { landmarkInfoState } from '../../atom';
+import { useNavigate } from 'react-router-dom';
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
+
+import * as API from '../../api';
+import { ValidationTextField } from './MuiCustom';
 
 import {
     UploadResultWrapper,
@@ -28,45 +35,127 @@ import Luggage from '../../img/Luggage.png';
 import img_4 from '../../img/landMark4.jpg';
 
 const UploadResult = () => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [restaurant, setRestaurant] = useState('test');
+    const [landmark_img, setLandmark_img] = useState('test');
+
+    const ref = useRef();
+    const navigate = useNavigate();
+    const Info = useRecoilValue(landmarkInfoState);
+    // console.log('랜드마크정보', Info);
+
+    console.log('board', title);
+    console.log('board', content);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await API.post('/board', {
+                title,
+                content,
+                restaurant,
+                landmark_img,
+            });
+            navigate('/Board');
+        } catch (err) {
+            console.log('err');
+        }
+    };
+
     return (
         <UploadResultWrapper>
-            <UploadResultLeft>
-                <ImgContainer src={img_4} />
-                <UploadResultContentContainer>
-                    <UploadResultContentInfoTitle>
-                        <div>사진 제목</div>
-                        <div>Date</div>
-                        <div>ID</div>
-                    </UploadResultContentInfoTitle>
-                    <UploadResultContentInfo>
-                        {' '}
-                        <div>블라블라.jpg</div>
-                        <div>22/12/2019</div>
-                        <div>By Elice</div>
-                    </UploadResultContentInfo>
-                    <UploadResultContentPeopleContainer>
-                        <UploadResultPeopleImg src={Luggage} />
-                        24명의 랜드마커들이 다녀갔습니다
-                    </UploadResultContentPeopleContainer>
-                </UploadResultContentContainer>
-                <UploadResultBtnContainer>
-                    <UploadResultBtn>게시하기</UploadResultBtn>
-                    <UploadResultBtn>기록하기</UploadResultBtn>
-                    <UploadResultBtn>뒤로가기</UploadResultBtn>
-                </UploadResultBtnContainer>
-            </UploadResultLeft>
-            <UploadResultRight>
+            <Flippy ref={ref} flipOnClick={false} flipDirection="horizontal">
+                <FrontSide style={{ padding: '0', boxShadow: 'none' }}>
+                    <UploadResultLeft>
+                        <ImgContainer src={img_4} />
+                        <UploadResultContentContainer>
+                            <UploadResultContentInfoTitle>
+                                <div>사진 제목</div>
+                                <div>Date</div>
+                                <div>ID</div>
+                            </UploadResultContentInfoTitle>
+                            <UploadResultContentInfo>
+                                {' '}
+                                <div>블라블라.jpg</div>
+                                <div>22/12/2019</div>
+                                <div>By Elice</div>
+                            </UploadResultContentInfo>
+                            <UploadResultContentPeopleContainer>
+                                <UploadResultPeopleImg src={Luggage} />
+                                24명의 랜드마커들이 다녀갔습니다
+                            </UploadResultContentPeopleContainer>
+                        </UploadResultContentContainer>
+                        <UploadResultBtnContainer>
+                            <UploadResultBtn
+                                onClick={() => {
+                                    ref.current.toggle();
+                                }}
+                            >
+                                게시글 작성하기
+                            </UploadResultBtn>
+                            <UploadResultBtn>기록하기</UploadResultBtn>
+                            <UploadResultBtn onClick={() => navigate('/Upload')}>
+                                뒤로가기
+                            </UploadResultBtn>
+                        </UploadResultBtnContainer>
+                    </UploadResultLeft>
+                </FrontSide>
+                <BackSide style={{ padding: '0', boxShadow: 'none' }}>
+                    <UploadResultLeft>
+                        <ImgContainer src={img_4} />
+                        <UploadResultContentContainer>
+                            <ValidationTextField
+                                id="outlined-basic"
+                                label="제목"
+                                variant="outlined"
+                                value={title}
+                                onChange={(e) => {
+                                    setTitle(e.target.value);
+                                }}
+                            />
+
+                            <ValidationTextField
+                                id="outlined-multiline-static"
+                                label="내용"
+                                value={content}
+                                multiline
+                                rows={6}
+                                onChange={(e) => {
+                                    setContent(e.target.value);
+                                }}
+                            />
+                        </UploadResultContentContainer>
+                        <UploadResultBtnContainer>
+                            <UploadResultBtn onClick={handleSubmit}>확인</UploadResultBtn>
+                            <UploadResultBtn
+                                onClick={() => {
+                                    ref.current.toggle();
+                                }}
+                            >
+                                취소
+                            </UploadResultBtn>
+                        </UploadResultBtnContainer>
+                    </UploadResultLeft>
+                </BackSide>
+            </Flippy>
+
+            <UploadResultRight Info={Info}>
                 <UploadResultNameContainer>
                     <UploadResultNameImg src={name} />
-                    랜드마크 이름: 블라블라
+                    {/* 랜드마크 이름: */}
+                    {Info.name}
                 </UploadResultNameContainer>
                 <UploadResultLocationContainer>
                     <UploadResultLocationImg src={location} />
-                    랜드마크 위치: 블라블라
+                    {/* 랜드마크 위치:  */}
+                    {Info.add}
                 </UploadResultLocationContainer>
                 <UploadResultDescriptionContainer>
                     <UploadResultDescriptionImg src={description} />
-                    랜드마크 설명: 블라블라
+                    {/* 랜드마크 설명:  */}
+                    {Info.description}
                 </UploadResultDescriptionContainer>
             </UploadResultRight>
         </UploadResultWrapper>
