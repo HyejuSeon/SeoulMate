5de0b6bab857b0332626d1472bac2b7e496b2db5
MongLong0214 <weplay0628@gmail.com>
2022-06-21 13:32:27 +0900

    test

--- front/src/components/board/BoardContent.js
+++ front/src/components/board/BoardContent.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { BoardContentContainer } from './BoardContentStyle';
+import { BoardContentContainer, ImgContainer } from './BoardContentStyle';
+import * as API from '../../api';
 
 const BoardContent = () => {
+    const [allBoardContent, setAllBoardContent] = useState('');
+
+    useEffect(() => {
+        const getBoardContent = async () => {
+            const res = await API.get('board/8411223d-c4ee-4f13-b4db-efb832b90ade');
+            const content = res.data;
+            setAllBoardContent(content);
+            console.log('content', content);
+        };
+        getBoardContent();
+    }, []);
+
     return (
         <>
-            <BoardContentContainer></BoardContentContainer>
+            <BoardContentContainer>
+                <ImgContainer src={allBoardContent.landmark_img_id} />
+            </BoardContentContainer>
         </>
     );
 };
