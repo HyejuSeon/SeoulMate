19f5d3d8d4d7849b0aeb9293695ef5194af53a3e
pjs <vkrwltn12@gmail.com>
2022-06-24 04:44:53 +0900

    feat : add kakaomap with lat(위도), lng(경도)

--- front/src/components/kakao/MapContainer.js
+++ front/src/components/kakao/MapContainer.js
@@ -1,27 +1,34 @@
-// MapContainer.js
+/*global kakao */
+import React, { useEffect } from "react";
+import { markerdata } from "./makerData";
 
-import React, { useEffect } from 'react';
+export default function Map() {
+  useEffect(() => {
+    mapscript();
+  }, []);
 
-const { kakao } = window;
+  const mapscript = () => {
+    let container = document.getElementById("map");
+    let options = {
+      center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
+      level: 5,
+    };
 
-const MapContainer = () => {
+    //map
+    const map = new kakao.maps.Map(container, options);
+    
+    markerdata.forEach((el) => {
+      // 마커를 생성합니다
+      new kakao.maps.Marker({
+        //마커가 표시 될 지도
+        map: map,
+        //마커가 표시 될 위치
+        position: new kakao.maps.LatLng(el.lat, el.lng),
+        //마커에 hover시 나타날 title
+        title: el.title,
+      });
+    });
+  };
 
-    useEffect(() => {
-        const container = document.getElementById('myMap');
-		const options = {
-			center: new kakao.maps.LatLng(33.450701, 126.570667),
-			level: 3
-		};
-        const map = new kakao.maps.Map(container, options);
-    }, []);
-
-    return (
-        <div id='myMap' style={{
-            width: '1000px', 
-            height: '400px',
-            marginLeft: '100px',
-        }}></div>
-    );
+  return <div id="map" style={{ width: "100vw", height: "100vh" }}></div>;
 }
-
-export default MapContainer; 
