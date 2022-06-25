294ccdd3063796700a5c3b24ac26fec650f19a2a
pjs <vkrwltn12@gmail.com>
2022-06-26 05:16:46 +0900

    feat: kakao map maker

--- front/src/components/kakao/MapContainer.js
+++ front/src/components/kakao/MapContainer.js
@@ -7,67 +7,88 @@ import { userInfoState } from '../../atom';
 
 export default function Map() {
   useEffect(() => {
-    mapscript();
+    userInfo();
   }, []);
 
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState();
 
 
-  // const user = useRecoilValue(userInfoState);
+
+  const userB = useRecoilValue(userInfoState);
   const [kakaouser, setkakaoUser] = useState();
 
-  const mapscript = () => {
-    let container = document.getElementById("map");
-    let options = {
-      center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
-      level: 5,
-    };
+//   const mapscript = () => {
+//     let container = document.getElementById("map");
+//     let options = {
+//       center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
+//       level: 5,
+//     };
 
   
-    //map
-    const map = new kakao.maps.Map(container, options);
+//     //map
+//     const map = new kakao.maps.Map(container, options);
     
-    markerdata.forEach((el) => {
-      // 마커를 생성합니다
-      new kakao.maps.Marker({
-        //마커가 표시 될 지도
-        map: map,
-        //마커가 표시 될 위치
-        position: new kakao.maps.LatLng(el.lat, el.lng),
-        //마커에 hover시 나타날 title
-        title: el.title,
-      });
-    });
-  };
+//     markerdata.forEach((el) => {
+//       // 마커를 생성합니다
+//       new kakao.maps.Marker({
+//         //마커가 표시 될 지도
+//         map: map,
+//         //마커가 표시 될 위치
+//         position: new kakao.maps.LatLng(el.lat, el.lng),
+//         //마커에 hover시 나타날 title
+//         title: el.title,
+//       });
+//     });
+//   };
 
   async function getUserData() {
     try {
-    const res = await API.get("users/current/info");
-    setUser(res.data.user_id)
+    const res = await API.getQuery(`visited?user_id=${userB.user_id}`);
+    setUser(res.data)
     } catch (err) {
     console.log("err");
     }
 }
 
-
-
 useEffect(() => {
-  const kakaoUser = () => {
-  try{
-  const res = API.getQuery('visited?user_id=d810a62e-7100-4fa8-9383-11c0cf9db695')
-  setkakaoUser(res.data) 
-  } catch(err) {
-    console.log("err");
-  }
-}
-
-  kakaoUser();
+  getUserData();
 }, []);
 
+// useEffect(()=>{
+//   try {
+//     const res = API.get("visited?user_id=d810a62e-7100-4fa8-9383-11c0cf9db695");
+//     setkakaoUser(res.data)
+//     } catch (err) {
+//     console.log("err");
+//     }
+// }, [])
 
 
-console.log("user:", user)  
-console.log("kakaouser:", kakaouser)  
+function userInfo () {
+  let container = document.getElementById("map");
+  let options = {
+    center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
+    level: 5,
+  };
+  if (user) {
+    const map = new kakao.maps.Map(container, options);
+    const b = {
+      ...user.payloads.forEach((el)=> {
+        new kakao.maps.Marker({
+          //마커가 표시 될 지도
+          map: map,
+          //마커가 표시 될 위치
+          position: new kakao.maps.LatLng(el["landmark"].latitude, el["landmark"].longitude),
+          //마커에 hover시 나타날 title
+          title: el.name,
+        });
+      }
+
+      )
+    }
+    console.log("서울마커:", b)
+  }
+}
 
 
 
