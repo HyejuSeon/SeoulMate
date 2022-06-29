95b6a6564b98e1f723c93848354c092a8d5c50e4
홍지운 <loans93@gmail.com>
2022-06-29 15:12:04 +0900

    feat:temp data fetch

--- back/src/ai/ai.controller.ts
+++ back/src/ai/ai.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, HttpStatus, Post, Res } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LandmarksService } from 'src/landmarks/landmarks.service';
 import { AiService } from './ai.service';
+import { insertAI } from './dto/insert.ai.dto';
 
 @ApiTags('ai')
 @Controller('ai')
@@ -12,17 +13,34 @@ export class AiController {
     ) {}
 
     @Post()
-    @ApiOperation({ summary: 'ai 분석 결과 반환' })
+    @ApiOperation({ summary: '목데이터 랜덤 결과 반환' })
     @ApiResponse({
         status: 200,
         description: 'image 분석 결과',
     })
-    async aiAnalyze(@Res() res: any): Promise<void> {
+    async aiRandom(@Res() res: any): Promise<void> {
         try {
             const visited = await this.landmarksService.getLandmarks();
             const randomIndex = Math.floor(Math.random() * visited[0].length);
 
             res.status(HttpStatus.OK).json(visited[0][randomIndex]);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    @Post()
+    @ApiOperation({ summary: 'ai 분석 결과 반환' })
+    @ApiBody({ type: insertAI })
+    @ApiResponse({
+        status: 200,
+        description: 'image 분석 결과',
+    })
+    async aiAnalyze(@Res() res: any, @Body() url: insertAI): Promise<void> {
+        try {
+            const result = await this.aiService.getData(url);
+            res.status(HttpStatus.OK).json(result);
+
         } catch (err) {
             console.log(err);
         }
--- back/src/ai/ai.service.ts
+++ back/src/ai/ai.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AiService {}
+export class AiService {
+    async getData(url) {
+        const response = await fetch("http://localhost:5002/", {
+            method: 'POST',
+            mode: 'cors',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ url: url }),
+        })
+        return response.json();
+    }
+}
